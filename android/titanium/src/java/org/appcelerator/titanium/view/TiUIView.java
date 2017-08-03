9a3e7524806f22c36903f859e7f6c0a113fe8bb0
Sergey Volkov <s.volkov@netris.ru>
2017-08-03 13:07:15 +0300

    fix children release on TiUIView
    based on d1ed57357d92de80ff10b3c1e6e66e17fa1ace39
    excluded changes on TiUIScrollView
    
    (cherry picked from commit bc9538d)

--- android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
@@ -1303,7 +1303,6 @@ public abstract class TiUIView implements KrollProxyListener, OnFocusChangeListe
 			nativeView.setOnTouchListener(null);
 			nativeView.setOnDragListener(null);
 			nativeView.setOnFocusChangeListener(null);
-			nativeView = null;
 			borderView = null;
 			if (proxy != null) {
 				proxy.setModelListener(null);
@@ -1311,11 +1310,20 @@ public abstract class TiUIView implements KrollProxyListener, OnFocusChangeListe
 		}
 		if (children != null) {
 			for (TiUIView child : children) {
-				remove(child);
+				if (child != null) {
+					View cv = child.getOuterView();
+					if (cv != null) {
+						if (nativeView instanceof ViewGroup) {
+							((ViewGroup) nativeView).removeView(cv);
+							child.parent = null;
+						}
+					}
+				}
 			}
 			children.clear();
 			children = null;
 		}
+		nativeView = null;
 		parent = null;
 		proxy = null;
 		layoutParams = null;
